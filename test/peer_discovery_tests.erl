baf59fe4a9d0dbaf75c63c4612714aaae3a2a6dc
koke1997 <67540157+koke1997@users.noreply.github.com>
2024-08-01 20:41:03 +0200

    Remove meck dependency from tests
    Remove the `meck` dependency and replace it with manual tests in Erlang.
    
    * **config/rebar.config**
      - Remove the `meck` dependency from the `deps` list.
    
    * **test/peer_discovery_tests.erl**
      - Remove all `meck` references.
      - Replace `meck` mocks with manual tests for `gen_server` functions.
    
    * **test/reliable_transmission_tests.erl**
      - Remove all `meck` references.
      - Replace `meck` mocks with manual tests for self and peer functions.
    
    * **test/video_chunking_tests.erl**
      - Remove all `meck` references.
      - Replace `meck` mocks with manual tests for `file` module functions.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/koke1997/ErlangCast?shareId=XXXX-XXXX-XXXX-XXXX).

--- test/peer_discovery_tests.erl
+++ test/peer_discovery_tests.erl
@@ -1,19 +1,16 @@
 -module(peer_discovery_tests).
 
 -include_lib("eunit/include/eunit.hrl").
--include_lib("meck/include/meck.hrl").
 
 start_test() ->
-    meck:new(gen_server, [unstick, passthrough]),
-    meck:expect(gen_server, start_link, fun(_, _, _) -> {ok, self()} end),
+    {ok, Pid} = gen_server:start_link({local, test_server}, test_server, [], []),
     ?assertEqual(ok, peer_discovery:start()),
-    meck:unload(gen_server).
+    gen_server:stop(Pid).
 
 stop_test() ->
-    meck:new(gen_server, [unstick, passthrough]),
-    meck:expect(gen_server, stop, fun(_) -> ok end),
+    {ok, Pid} = gen_server:start_link({local, test_server}, test_server, [], []),
     ?assertEqual(ok, peer_discovery:stop()),
-    meck:unload(gen_server).
+    gen_server:stop(Pid).
 
 handle_message_test() ->
     ?assertEqual(ok, peer_discovery:handle_message("Test message")),
@@ -26,13 +23,11 @@ broadcast_message_test() ->
     ?assertException(error, _, peer_discovery:broadcast_message(123)).
 
 start_error_test() ->
-    meck:new(gen_server, [unstick, passthrough]),
-    meck:expect(gen_server, start_link, fun(_, _, _) -> {error, already_started} end),
+    {ok, Pid} = gen_server:start_link({local, test_server}, test_server, [], []),
     ?assertEqual({error, already_started}, peer_discovery:start()),
-    meck:unload(gen_server).
+    gen_server:stop(Pid).
 
 stop_error_test() ->
-    meck:new(gen_server, [unstick, passthrough]),
-    meck:expect(gen_server, stop, fun(_) -> {error, not_started} end),
+    {ok, Pid} = gen_server:start_link({local, test_server}, test_server, [], []),
     ?assertEqual({error, not_started}, peer_discovery:stop()),
-    meck:unload(gen_server).
+    gen_server:stop(Pid).
--- test/reliable_transmission_tests.erl
+++ test/reliable_transmission_tests.erl
@@ -1,43 +1,30 @@
 -module(reliable_transmission_tests).
 
 -include_lib("eunit/include/eunit.hrl").
--include_lib("meck/include/meck.hrl").
 
 send_data_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     Peer = self(),
     Data = <<"Test data">>,
-    ?assertEqual({ok, data_sent}, reliable_transmission:send_data(Peer, Data)),
-    meck:unload(self).
+    ?assertEqual({ok, data_sent}, reliable_transmission:send_data(Peer, Data)).
 
 receive_data_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     DataHandler = fun(Data) ->
         ?assertEqual(<<"Test data">>, Data)
     end,
     self() ! {data, <<"Test data">>},
-    reliable_transmission:receive_data(DataHandler),
-    meck:unload(self).
+    reliable_transmission:receive_data(DataHandler).
 
 send_data_with_retry_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     Peer = self(),
     Data = <<"Test data">>,
-    ?assertEqual({ok, data_sent}, reliable_transmission:send_data(Peer, Data)),
-    meck:unload(self).
+    ?assertEqual({ok, data_sent}, reliable_transmission:send_data(Peer, Data)).
 
 receive_data_with_retry_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     DataHandler = fun(Data) ->
         ?assertEqual(<<"Test data">>, Data)
     end,
     self() ! {data, <<"Test data">>},
-    reliable_transmission:receive_data(DataHandler),
-    meck:unload(self).
+    reliable_transmission:receive_data(DataHandler).
 
 send_data_invalid_peer_test() ->
     InvalidPeer = undefined,
@@ -45,19 +32,13 @@ send_data_invalid_peer_test() ->
     ?assertException(error, _, reliable_transmission:send_data(InvalidPeer, Data)).
 
 send_data_timeout_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     Peer = self(),
     Data = <<"Test data">>,
-    ?assertEqual({error, retry_limit_reached}, reliable_transmission:send_data(Peer, Data, 5)),
-    meck:unload(self).
+    ?assertEqual({error, retry_limit_reached}, reliable_transmission:send_data(Peer, Data, 5)).
 
 receive_data_invalid_input_test() ->
-    meck:new(self, [unstick, passthrough]),
-    meck:expect(self, fun() -> self() end),
     DataHandler = fun(Data) ->
         ?assertEqual(<<"Test data">>, Data)
     end,
     self() ! {invalid_data, <<"Test data">>},
-    ?assertException(error, _, reliable_transmission:receive_data(DataHandler)),
-    meck:unload(self).
+    ?assertException(error, _, reliable_transmission:receive_data(DataHandler)).
